465926fbfa80141e7ba6fbb8189e447613f2ee37
Ben Simpson <benjo696@gmail.com>
2022-08-10 14:41:29 +0100

    install xk6browser

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,8 @@
 go install go.k6.io/xk6/cmd/xk6@latest
+
+echo "GOROOT=$HOME/sdk/gotip" >>"$GITHUB_ENV"
+echo "GOPATH=$HOME/go" >>"$GITHUB_ENV"
+echo "$HOME/go/bin" >>"$GITHUB_PATH"
+echo "$HOME/sdk/gotip/bin" >>"$GITHUB_PATH"
+
 xk6 build --output xk6-browser --with github.com/grafana/xk6-browser
