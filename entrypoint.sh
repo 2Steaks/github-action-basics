a33fa76599044166c4f6c537d3b4de51c57d24ef
Ben Simpson <benjo696@gmail.com>
2022-08-11 10:05:20 +0100

    install xk6browser

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,19 @@
 
 set -x
 export XK6_HEADLESS=true
-export TEST_PATH_REGEX=${1}
+export $INPUT_TESTREGEX
 export $GITHUB_WORKSPACE/$TEST_PATH_REGEX
 
-for file in $GITHUB_WORKSPACE/$TEST_PATH_REGEX; do
-    ./xk6-browser run "$file"
+function has_file {
+    if [ ! -f "$1" ]; then
+        echo "$1 does not exist."
+    fi
+}
+
+has_file $HOME/xk6-browser
+has_file $GITHUB_WORKSPACE/xk6-browser
+has_file $GITHUB_PATH/xk6-browser
+
+for file in $INPUT_TESTREGEX; do
+    ./xk6-browser run "$GITHUB_WORKSPACE/$file"
 done
