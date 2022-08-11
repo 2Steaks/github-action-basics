61f4d013698d11ff1ae95858b0abab747ab95f9f
Ben Simpson <benjo696@gmail.com>
2022-08-11 10:42:03 +0100

    install xk6browser

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 set -x
 export XK6_HEADLESS=true
 export XK6_BROWSER=/go/xk6-browser
+export TEST_FILES=find $GITHUB_WORKSPACE/$INPUT_TESTREGEX
 
 for file in $GITHUB_WORKSPACE/$INPUT_TESTREGEX; do
     $XK6_BROWSER run "$file"
