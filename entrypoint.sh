fe01574517a923619ab554907cc610e9d4daa245
Ben Simpson <benjo696@gmail.com>
2022-08-11 12:55:57 +0100

    install xk6browser

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
+shopt -s globstar
+
 set -x
 XK6_HEADLESS=true
 XK6_BROWSER=/go/xk6-browser
-TEST_FILES=find $GITHUB_WORKSPACE/$INPUT_TESTREGEX
 
-echo $GITHUB_WORKSPACE/$INPUT_TESTREGEX
-
-echo $TEST_FILES
-
-for file in $TEST_FILES; do
+for file in $INPUT_INCLUDE; do
     $XK6_BROWSER run "$file"
 done
