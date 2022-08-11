3d29bb0769b91e02c4e56771a3922702955de998
Ben Simpson <benjo696@gmail.com>
2022-08-11 10:29:16 +0100

    install xk6browser

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,18 +4,6 @@ set -x
 export XK6_HEADLESS=true
 export XK6_PATH=/go/xk6-browser
 
-function has_directory {
-    if [ ! -d "$1" ]; then
-        echo "$1 does not exist."
-    fi
-}
-
-has_directory tests
-has_directory /tests
-has_directory /github/workspace/tests
-has_directory $GITHUB_WORKSPACE/tests
-has_directory /$GITHUB_WORKSPACE/tests
-
-for file in $GITHUB_WORKSPACE/$INPUT_TESTREGEX; do
-    $XK6_PATH run "$file"
+for file in $INPUT_TESTREGEX; do
+    $XK6_PATH run "$GITHUB_WORKSPACE/$file"
 done
